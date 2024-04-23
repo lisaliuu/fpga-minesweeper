4becae1426515a814500c4cbc6e575428bd2a00d
Junyeong Heo <hhjjyy1122@gmail.com>
2024-04-23 17:28:49 -0500

    2024-04-23
    Co-authored-by: Lisa Liu <lisaliuu@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+- edit
+  - repo/minesweeper/BOARD.vhd
+  - repo/minesweeper/DE2_115_TOP.vhd
+  - repo/minesweeper/LCD_Display.vhd
+  - repo/minesweeper/logic.vhd
+- don't edit
+  - repo/minesweeper/VGA_SYNC.VHD
+  - repo/minesweeper/video_PLL.vhd
--- repo/minesweeper/logic.vhd
+++ repo/minesweeper/logic.vhd
@@ -1,205 +1,110 @@
--- Logic Block Entity
--- Designed by Michael Delaney, Jenna Kronenberg
--- EECE 4377: FPGA Design
--- April 19, 2022
-
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
-USE work.ALL;
-USE work.board_layout_pkg.ALL;
+
+-- TYPE board_status IS ARRAY(0 TO 7, 0 TO 7); -- bool
+-- TYPE board_value IS ARRAY(0 TO 7, 0 TO 7); -- int from 0 to 8, 9 (bomb), 10 (flag)
+
+-- determine the status of the cell
+-- input: IN ports <- user input (buttons, switches)
+-- output
 
 ENTITY logic IS
 
 	PORT (
 		clk : IN STD_LOGIC;
-		VGA_RDY : IN STD_LOGIC;
 		reset : IN STD_LOGIC;
-		buttons : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
-		switches : IN STD_LOGIC_VECTOR(6 DOWNTO 0);
+		-- user input
+		buttons : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+		switches : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
 
 		VGA_UPDATE : OUT STD_LOGIC;
-		currentPlayer : BUFFER STD_LOGIC;
-		nextPieceColumn : BUFFER unsigned(6 DOWNTO 0);
-		boardLayout : BUFFER BOARD_SCHEME;
-		winner : OUT STD_LOGIC
+		cell_status : BUFFER ARRAY(0 TO 7, 0 TO 7) OF STD_LOGIC;
+		cell_flagged : BUFFER ARRAY(0 TO 7, 0 TO 7) OF STD_LOGIC;
+		cell_value : BUFFER ARRAY(0 TO 7, 0 TO 7) OF INTEGER;
+		cur_sel_cell : BUFFER ARRAY(0 TO 1) OF INTEGER;
 
 	);
+END ENTITY logic;
 
-END logic;
 
-ARCHITECTURE ARCH OF logic IS
 
-	SIGNAL prevButtons : STD_LOGIC_VECTOR(2 DOWNTO 0);
+ARCHITECTURE architecture OF logic IS
+
+	SIGNAL game_over : STD_LOGIC;
+	SIGNAL first_pressed : STD_LOGIC := "0";
+	SIGNAL check_win : STD_LOGIC := "1";
 
 BEGIN
-
-	--nextPieceColumn <= unsigned(switches(6 downto 0));
-
-	PROCESS (clk, reset)
-
-		VARIABLE dropColumn : INTEGER := 0;
-		VARIABLE dropRow : INTEGER := 0;
-		VARIABLE winDetector : INTEGER := 0;
-
+	PROCESS (switches, buttons)
+	-- switch 0 == 0 for playing
+	-- switch 0 == 1 for game over/reset
 	BEGIN
-
-		IF (reset = '1') THEN
-
-			-- Do the same thing as "display board"
-			VGA_UPDATE <= '1';
-			currentPlayer <= '0';
-			prevButtons <= buttons;
-			nextPieceColumn <= "1000000";
-			winner <= '0';
-
-			FOR i IN 0 TO 6 LOOP -- Column
-				FOR j IN 0 TO 5 LOOP -- Row
-					boardLayout(i, j) <= empty;
+	-- generate a new board when sw = 1
+	
+	-- start the game when sw=0	
+	-- initialize blank board
+		VGA_UPDATE <= '1';
+		IF (switches(0) = '0') THEN
+			cur_sel_cell <= (0, 0)
+			FOR i IN 0 TO 7 LOOP -- Column
+				FOR j IN 0 TO 7 LOOP -- Row
+					cell_status(i, j) <= "0";
+					cell_flagged(i, j) <= "0";
 				END LOOP;
 			END LOOP;
-
-		ELSIF (rising_edge(clk) AND prevButtons /= buttons) THEN
-
-			CASE buttons IS
-
-				WHEN "110" =>
-					-- Shift right
-					CASE nextPieceColumn IS
-						WHEN "1000000" => nextPieceColumn <= "0100000";
-						WHEN "0100000" => nextPieceColumn <= "0010000";
-						WHEN "0010000" => nextPieceColumn <= "0001000";
-						WHEN "0001000" => nextPieceColumn <= "0000100";
-						WHEN "0000100" => nextPieceColumn <= "0000010";
-						WHEN "0000010" => nextPieceColumn <= "0000001";
-						WHEN "0000001" => nextPieceColumn <= "1000000";
-						WHEN OTHERS => nextPieceColumn <= "1000000";
-					END CASE;
-					VGA_UPDATE <= '0';
-
-				WHEN "101" =>
-					CASE nextPieceColumn IS
-						WHEN "1000000" =>
-							dropColumn := 0;
-						WHEN "0100000" =>
-							dropColumn := 1;
-						WHEN "0010000" =>
-							dropColumn := 2;
-						WHEN "0001000" =>
-							dropColumn := 3;
-						WHEN "0000100" =>
-							dropColumn := 4;
-						WHEN "0000010" =>
-							dropColumn := 5;
-						WHEN "0000001" =>
-							dropColumn := 6;
-						WHEN OTHERS =>
-							-- ERROR!
-							dropColumn := - 1;
-					END CASE;
-					-- Update the board layout
-					IF (dropColumn /= - 1) THEN
-
-						FOR i IN 0 TO 5 LOOP
-							IF (boardLayout(dropColumn, i) = empty) THEN
-								dropRow := i;
-							END IF;
-						END LOOP;
-
-						IF (boardLayout(dropColumn, dropRow) = empty) THEN
-							IF (currentPlayer = '0') THEN
-								boardLayout(dropColumn, dropRow) <= player_1;
-							ELSE
-								boardLayout(dropColumn, dropRow) <= player_2;
-							END IF;
-
-							VGA_UPDATE <= '0';
-							currentPlayer <= NOT(currentPlayer);
-						END IF;
-					END IF;
-
-					--Win detection (vertical)
-					winDetector := 1;
-					FOR i IN -3 TO 3 LOOP
-						IF ((dropRow + i) >= 0) AND ((dropRow + i) < 6) THEN
-							IF (currentPlayer = '0' AND boardLayout(dropColumn, (dropRow + i)) = player_1) OR
-								(currentPlayer = '1' AND boardLayout(dropColumn, (dropRow + i)) = player_2) THEN
-								winDetector := winDetector + 1;
-								IF (winDetector >= 4) THEN
-									winner <= '1';
-								END IF;
-							END IF;
-						END IF;
-					END LOOP;
-
-					--Win detection (horizontal)
-					winDetector := 1;
-					FOR i IN -3 TO 3 LOOP
-						IF ((dropColumn + i) >= 0) AND ((dropColumn + i) < 7) THEN
-							IF (currentPLayer = '0' AND boardLayout((dropColumn + i), dropRow) = player_1) OR
-								(currentPlayer = '1' AND boardLayout((dropColumn + i), dropRow) = player_2) THEN
-								winDetector := winDetector + 1;
-								IF (winDetector >= 4) THEN
-									winner <= '1';
-								END IF;
-							END IF;
-						END IF;
-					END LOOP;
-
-					--Win detection (diagonal right)
-					winDetector := 1;
-					FOR i IN -3 TO 3 LOOP
-						IF ((dropColumn + i) >= 0) AND ((dropColumn + i) < 7) AND
-							((dropRow + i) >= 0) AND ((dropRow + i) < 6) THEN
-							IF (currentPlayer = '0' AND boardLayout((dropColumn + i), (dropRow + i)) = player_1) OR
-								(currentPlayer = '1' AND boardLayout((dropColumn + i), (dropRow + i)) = player_2) THEN
-								winDetector := winDetector + 1;
-								IF (winDetector >= 4) THEN
-									winner <= '1';
-								END IF;
-							END IF;
-						END IF;
-					END LOOP;
-
-					--Win detection (diagonal left)
-					winDetector := 1;
-					FOR i IN -3 TO 3 LOOP
-						IF ((dropColumn + i) >= 0) AND ((dropColumn + i) < 7) AND
-							((dropRow - i) >= 0) AND ((dropRow - i) < 6) THEN
-							IF (currentPlayer = '0' AND boardLayout((dropColumn + i), (dropRow - i)) = player_1) OR
-								(currentPlayer = '1' AND boardLayout((dropColumn + i), (dropRow - i)) = player_2) THEN
-								winDetector := winDetector + 1;
-								IF (winDetector >= 4) THEN
-									winner <= '1';
-								END IF;
-							END IF;
-						END IF;
-					END LOOP;
-
-				WHEN "011" =>
-
-					CASE nextPieceColumn IS
-						WHEN "1000000" => nextPieceColumn <= "0000001";
-						WHEN "0100000" => nextPieceColumn <= "1000000";
-						WHEN "0010000" => nextPieceColumn <= "0100000";
-						WHEN "0001000" => nextPieceColumn <= "0010000";
-						WHEN "0000100" => nextPieceColumn <= "0001000";
-						WHEN "0000010" => nextPieceColumn <= "0000100";
-						WHEN "0000001" => nextPieceColumn <= "0000010";
-						WHEN OTHERS => nextPieceColumn <= "1000000";
-					END CASE;
-
-					VGA_UPDATE <= '0';
-
-				WHEN OTHERS =>
-					VGA_UPDATE <= '1';
-
-			END CASE;
-
-			prevButtons <= buttons;
-
+		ENd IF;
+			
+		
+		-- ←↑↓→
+		IF (buttons(0)="0") THEN
+		-- go left
+			if cur_sel_cell(0) then
+			end if ;
+			cur_sel_cell(0) <= cur_sel_cell(0) - 1
+		-- go right
+		ELIF (buttons(1)="0") THEN
+			cur_sel_cell(0) <= cur_sel_cell(0) + 1
+		-- go down
+		ELIF (buttons(2)="0") THEN
+			cur_sel_cell(1)<= cur_sel_cell(1) - 1
+		-- go up
+		ELIF (buttons(3)="0") THEN
+			cur_sel_cell(1) <= cur_sel_cell(1) + 1
 		END IF;
 
-	END PROCESS;
+		IF (switches(1)="1") THEN
+			-- click (closed -> open)
+			IF (first_pressed="0") THEN
+				--first press
+				-- @TEYON PUT UR STUFF HERE
 
-END ARCH;
+				-- board is now partially open
+				first_pressed="1"
+			-- ELSE 
+			END if
+
+			cell_status(cur_sel_cell(0)*8+cur_sel_cell(1)) <= "1"
+			IF (cell_value(cur_sel_cell(0)*8+cur_sel_cell(1)) = "9") THEN -- hit a bomb
+				game_over = "1"; -- lost
+			END IF
+
+			FOR i IN 0 TO 7 LOOP -- Column
+				FOR j IN 0 TO 7 LOOP -- Row
+					IF (NOT (cell_status(i*8+j)="1" or cell_flagged(i*8+j)="1")) THEN
+						check_win <= "0"
+
+			IF (check_win="1") then
+				game_over = "1" -- won
+			END IF
+
+		ELIF (switches(2)="1") THEN
+			-- flag
+			IF(cell_status(cur_sel_cell(0)*8+cur_sel_cell(1))="0") THEN
+				cell_flagged(cur_sel_cell(0)*8+cur_sel_cell(1)) <= "1"
+			ELSE
+				cell_flagged(cur_sel_cell(0)*8+cur_sel_cell(1)) <= "0"
+			END IF
+		END IF;
+	END PROCESS;
+END ARCHITECTURE;
